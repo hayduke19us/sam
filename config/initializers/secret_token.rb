a54a86bb38dd7fd7db30baa7ba7f36166309bf95
hayduke19us <hayduke19us@gmail.com>
2014-03-30 15:54:10 -0500

    devise is set up with a simple login email and password. database is within the vendor file(postgresql). Foundation-rails gem for styling. Minitest for test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sam::Application.config.secret_key_base = '7c1bf19f55f5be892701e4d7814cd9ed8c29e537108100e6997b349b1637132827cd01d4509a4eb8f82d1286ac80f9f19f69df344a354f25cc69efd8bf3a57b7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sam::Application.config.session_store :cookie_store, key: '_sam_session'
