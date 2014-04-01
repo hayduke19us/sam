cacd43c832085eb150fa51329f1bab76987a6b84
hayduke19us <hayduke19us@gmail.com>
2014-04-01 12:40:28 -0500

    created a migration to remove lat and long from profiles because it is now apart of user

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140401172919) do
+ActiveRecord::Schema.define(version: 20140401173914) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "profiles", force: true do |t|
     t.string   "religion"
-    t.float    "latitude"
-    t.float    "longitude"
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
