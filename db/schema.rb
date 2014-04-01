076b6008f4af3093c8756b7a022f551523a0f8fb
hayduke19us <hayduke19us@gmail.com>
2014-04-01 12:31:02 -0500

    removed ip_adress from table profile

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140401171728) do
+ActiveRecord::Schema.define(version: 20140401172919) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,6 @@ ActiveRecord::Schema.define(version: 20140401171728) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "ip_address"
   end
 
   create_table "users", force: true do |t|
