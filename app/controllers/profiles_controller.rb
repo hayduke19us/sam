2fd222455f9f5f3f1ed81a981e5ac85dc11b3831
hayduke19us <hayduke19us@gmail.com>
2014-04-01 12:30:22 -0500

    created migration to remove ip_address from profiles

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,8 +1,7 @@
 class ProfilesController < ApplicationController
 
   def create
-    profile = Profile.new(ip_address: current_user.current_signed_in_ip,
-                          religion: params[:religion],
+    profile = Profile.new(religion: params[:religion],
                           user_id: current_user.id)
     if profile.save
       redirect_to users_dashboard_path
