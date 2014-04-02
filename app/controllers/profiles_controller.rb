a46c4d3578d600cecfdae3ca735ef15ba997fd72
hayduke19us <hayduke19us@gmail.com>
2014-04-02 00:14:30 -0500

    added a correct your city input to the create profile modal

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,9 +1,13 @@
 class ProfilesController < ApplicationController
 
   def create
-    profile = Profile.new(ip_address: request.remote_ip,
-                          religion: params[:religion],
+    profile = Profile.new(religion: params[:religion],
                           user_id: current_user.id)
+    if params[:city]
+      current_user.city = params[:city]
+      current_user.save
+    end
+
     if profile.save
       redirect_to users_dashboard_path
     end
--- test/models/profile_test.rb
+++ test/models/profile_test.rb
@@ -6,11 +6,6 @@ class ProfileTest < ActiveSupport::TestCase
     @franny_profile = profiles(:franny_profile)
   end
 
-  test "a profile without latitude is invalid" do
-    @franny_profile.ip_address = nil
-    refute @franny_profile.valid?
-  end
-
   test "a user is accessible from profile" do
     franny = users(:franny)
     assert_equal franny, @franny_profile.user 
