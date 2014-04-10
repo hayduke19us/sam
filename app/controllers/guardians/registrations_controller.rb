981791634480bd0c3b1ce2619f5e19bcbfee4ca0
hayduke19us <hayduke19us@gmail.com>
2014-04-09 22:42:14 -0500

    removed the customization to sanitizing the user_id column because i don't need to access that from the view

--- app/controllers/guardians/registrations_controller.rb
+++ app/controllers/guardians/registrations_controller.rb
@@ -1,26 +1,9 @@
 class Guardians::RegistrationsController < Devise::RegistrationsController
 
-  def new
-    super
-  end
-
   def create
     super do |resource|
       resource.user_id = current_user.id
     end
   end
-
-  def update
-    super
-  end
-
-  private
-
-    def sign_up_params
-      params.require(:guardian).permit( :email,
-                                        :password,
-                                        :password_confirmation,
-                                        :user_id )
-    end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,6 @@ class User < ActiveRecord::Base
   has_one :profile
   has_one :guardian
 
-  validates_presence_of :email, :encrypted_password
-
   geocoded_by :ip_address, latitude: :lat, longitude: :long
 
   reverse_geocoded_by :lat, :long do |obj, results|
@@ -19,6 +17,10 @@ class User < ActiveRecord::Base
   end
 
   after_validation :geocode, :reverse_geocode
+
+  #The actual User attribute current_sign_in_ip will be unchanged.
+  #Geocode will inject the values of :lat and :long based off
+  #of the spoofed ip when in develoopment or test enviroments.
 
   def ip_address
     if Rails.env == "development" || Rails.env == "test"
