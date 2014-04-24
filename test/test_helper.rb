95683cfa8494fd7d76d3b36a8f52c791d678c1f3
hayduke19us <hayduke19us@gmail.com>
2014-04-24 12:55:46 -0500

    test_helper issue

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,13 +1,10 @@
+
 ENV["RAILS_ENV"] = "test"
 require File.expand_path("../../config/environment", __FILE__)
-
-require 'simplecov'
-Simplecove.start
 
 require 'coveralls'
 Coveralls.wear!('rails')
 
-require 'simplecov'
 
 require "rails/test_help"
 require "minitest/rails"
