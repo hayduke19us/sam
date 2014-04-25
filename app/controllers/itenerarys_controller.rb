dd3582bfd17719f3f09729035d88c351cd5fa6e8
hayduke19us <hayduke19us@gmail.com>
2014-04-25 14:20:28 -0500

    itenerary show view will have destination buttons

--- app/controllers/itenerarys_controller.rb
+++ app/controllers/itenerarys_controller.rb
@@ -1,6 +1,14 @@
 class ItenerarysController < ApplicationController
   def show
     @itenerary = Itenerary.find(params[:id])
+    @journey = @itenerary.journey
+    interactions = @itenerary.interactions
+    array = []
+    interactions.each do |inter|
+      array << inter.user
+    end
+    @destinations = array.map {|user| user.city }
+
   end
 
   def update
