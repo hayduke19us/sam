6524719a95976ffa937a9a65bf3a813c347abffd
hayduke19us <hayduke19us@gmail.com>
2014-05-02 01:16:36 -0500

    cleaned up test. removed helper test that were not completed

--- test/controllers/interactions_controller_test.rb
+++ test/controllers/interactions_controller_test.rb
@@ -1,9 +1,5 @@
 require "test_helper"
 
 class InteractionsControllerTest < ActionController::TestCase
-  def test_symposium
-    get :symposium
-    assert_response :success
-  end
 
 end
--- test/helpers/interactions_helper_test.rb
+++ test/helpers/interactions_helper_test.rb
@@ -2,8 +2,5 @@ require "test_helper"
 
 class InteractionsHelperTest < ActionView::TestCase
 
-  def test_sanity
-    flunk "Need real tests"
-  end
 
 end
